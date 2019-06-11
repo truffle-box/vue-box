fef0f86c0848652e8346f67bc1c8e7e010b04f9e
Amal Sudama <cds.sudama@gmail.com>
2019-06-11 09:57:03 -0400

    initial commit

---
+++ vapp/src/SimpleStorage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="isDrizzleInitialized">
+    <div>
+      <drizzle-contract
+        contractName="SimpleStorage"
+        method="storedData"
+        label="Value"
+      />
+      <drizzle-contract-form
+        contractName="SimpleStorage"
+        method="set"
+        :placeholders="['Value']"
+      />
+    </div>
+  </div>
+
+  <div v-else>Loading...</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'SimpleStorage',
+
+  computed: mapGetters('drizzle', ['isDrizzleInitialized'])
+}
+</script>
+
+<style></style>
---
+++ vapp/src/TutorialToken.vue
@@ -0,0 +1,50 @@
+<template>
+  <div v-if="isDrizzleInitialized">
+    <drizzle-contract
+      contractName="TutorialToken"
+      method="totalSupply"
+      label="Total"
+    />
+    <drizzle-contract
+      contractName="TutorialToken"
+      method="symbol"
+      label="Symbol"
+    />
+    <drizzle-contract
+      contractName="TutorialToken"
+      method="balanceOf"
+      label="Your Balance"
+      :methodArgs="accounts"
+    />
+
+    <drizzle-contract-form
+      contractName="TutorialToken"
+      method="transfer"
+      :placeholders="placeholders"
+    />
+  </div>
+
+  <div v-else>Loading...</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'TutorialToken',
+  computed: {
+    ...mapGetters('accounts', ['activeAccount', 'activeBalance']),
+    ...mapGetters('drizzle', ['isDrizzleInitialized']),
+
+    accounts() {
+      return [this.activeAccount]
+    },
+
+    placeholders() {
+      return ['To Address', 'Amount to Send']
+    }
+  }
+}
+</script>
+
+<style></style>
